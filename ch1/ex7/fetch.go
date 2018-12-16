c5d3b4ef70a08cddea2aedc5acb2214db4ce8895
nenuon <>
2018-12-16 15:14:21 +0900

    練習問題1.7を解いた

---
+++ ch1/ex7/fetch.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b := new(bytes.Buffer)
+		_, err = io.Copy(b, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b.String())
+	}
+}
