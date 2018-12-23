a57c0dd9924adf7f7cd4c7934fe4ea812aac7fce
nenuon <>
2018-12-23 17:47:39 +0900

    練習問題3.7を解いた

---
+++ ch3/ex7/mandelbrot.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+	png.Encode(os.Stdout, img)
+}
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = z - f(z)/g(z)
+		if cmplx.Abs(v-z) < 1e-5 {
+			return color.Gray{255 - contrast*n}
+		}
+		z = v
+	}
+	return color.Black
+}
+
+func f(z complex128) complex128 {
+	return z*z*z*z - 1
+}
+
+func g(z complex128) complex128 {
+	return 4 * z * z * z
+}
