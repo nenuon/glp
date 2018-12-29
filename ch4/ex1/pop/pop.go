e0320581bd7130edc70249bfa060c0d36db44662
nenuon <>
2018-12-29 23:47:38 +0900

    未使用の関数削除

--- ch4/ex1/pop/pop.go
+++ ch4/ex1/pop/pop.go
@@ -2,12 +2,6 @@ package pop
 
 var pc [256]byte
 
-func init() {
-	for i := range pc {
-		pc[i] = pc[i/2] + byte(i&1)
-	}
-}
-
 func SamePopCount(x, y uint64) int {
 	ret := 0
 	for i := uint(0); i < 64; i++ {
