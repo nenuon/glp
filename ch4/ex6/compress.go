0870f97c62d8393c3c2ce9950aafca4d52376683
nenuon <>
2018-12-31 23:39:35 +0900

    修正

--- ch4/ex6/compress.go
+++ ch4/ex6/compress.go
@@ -5,17 +5,44 @@ import (
 )
 
 func main() {
-	b := []byte("a       b         c        d")
+	b := []byte("       a\tb\rc\vd")
+	fmt.Printf("%s\n", b)
 	fmt.Printf("%s\n", compress(b))
 }
 
 func compress(bytes []byte) []byte {
-	r := bytes[:1]
-	for i := 1; i < len(bytes); i++ {
-		if bytes[i] == ' ' && r[len(r)-1] == ' ' {
+	r := bytes[:0]
+	for i := 0; i < len(bytes); i++ {
+		if isSpace(bytes[i]) {
+			if i == 0 || r[len(r)-1] != ' ' {
+				r = append(r, ' ')
+			}
 			continue
 		}
 		r = append(r, bytes[i])
 	}
 	return r
+}
+
+func isSpace(b byte) bool {
+	switch b {
+	case '\t':
+		return true
+	case '\n':
+		return true
+	case '\v':
+		return true
+	case '\f':
+		return true
+	case '\r':
+		return true
+	case ' ':
+		return true
+	case 0x85:
+		return true
+	case 0xa0:
+		return true
+	default:
+		return false
+	}
 }
