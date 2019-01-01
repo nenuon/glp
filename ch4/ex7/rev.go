31fbb9b2855668bf3a67178b2285ca4843ed4b45
nenuon <>
2019-01-01 13:58:36 +0900

    練習問題4.7を解いた

---
+++ ch4/ex7/rev.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []byte("abcde")
+	reverse(s)
+	fmt.Printf("%s\n", s)
+}
+
+func reverse(bytes []byte) {
+	for i, j := 0, len(bytes)-1; i < j; i, j = i+1, j-1 {
+		bytes[i], bytes[j] = bytes[j], bytes[i]
+	}
+}
