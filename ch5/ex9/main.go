f9123e64b03b6723d5a095be369591767ac4c2ee
nenuon <>
2019-01-19 17:20:02 +0900

    コメント追加

--- ch5/ex9/main.go
+++ ch5/ex9/main.go
@@ -12,6 +12,8 @@ func main() {
 
 // $がついた単語をf()で置換する
 // ex. `hello $world` -> `hello f(world)`
+// orisanoさんの方が良い!!!!
+// https://github.com/orisano/gopl/blob/master/ch05/ex09/main.go
 func expand(s string, f func(string) string) string {
 	words := strings.Split(s, " ")
 	result := make([]string, 0, len(words))
